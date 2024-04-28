e0143933360ad5c497d32d150b65f1004b498cac
arslan0044 <arslan.saqib121@gmail.com>
2024-04-28 19:35:43 +0500

    Update

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 // Connect to the MongoDB server using the environment variable
 mongoose.connect(
-    'mongodb+srv://businessman2118:Just@passw0rd@assignment-work.p4zhewp.mongodb.net/',
+    'mongodb+srv://adminuser:nGogT314RksQTGOX@cluster0.9ya7xfn.mongodb.net/',
     // "mongodb+srv://businessman2118:Just@passw0rd@assignment-work.p4zhewp.mongodb.net/?retryWrites=true&w=majority&appName=assignment-work",
     // 'mongodb+srv://businessman2118:Just@passw0rd@assignment-work.p4zhewp.mongodb.net/?retryWrites=true&w=majority&appName=assignment-work',
 // "mongodb://localhost:27017/newproject",
